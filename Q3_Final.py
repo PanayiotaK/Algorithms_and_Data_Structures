8941358403a88f8bfab6482f3d895d480844486c
PanayiotaK <panagiota.konstantinou@durham.ac.uk>
2019-01-17 23:48:26 +0000

    final

--- Q3_Final.py
+++ Q3_Final.py
@@ -58,156 +58,102 @@ class Queue:
             self.rear = self.rear.after
     
 #####################################################
-            
-
-def testq3():
-    assert good_expression("1+2+3+4")
-    assert good_expression("1*2*3*4")
-    assert not good_expression("(1+2+3+4)")
-    assert not good_expression("(1*2*3*4)") 
-    assert good_expression("(1+2)*3+4") 
-    assert not good_expression("((1+2))*3+4") 
-    assert good_expression("1+2*3+4") 
-    assert not good_expression("1+(2*3)+4") 
-    assert good_expression("1*2+3+4") 
-    assert not good_expression("1*2+(3+4)")
-    assert  good_expression("(1+2*3*((4+5)*6+7)+8)*9")
-    assert not good_expression("((1+2))*3+4")
-    assert not good_expression("1+(2*3)+4")
-    assert not good_expression("1* 2+(3+4)")
-    assert not good_expression("(1*2)+(2+2)")
-    assert not good_expression("(1*2)*(2+2)")
-    assert not good_expression("(1+2)*(2)")
-    assert not good_expression("(1+2)+(2)")
-    assert not good_expression("(1+2)+(2*3)")
-    assert not good_expression("(1+2)*(2*2)")
-    assert good_expression("(1+2)*(2+1)")
-    assert not good_expression("(1+2)(2+2)")
-    assert not good_expression("(1*2)+(3)")
-    assert not good_expression("(1*2)*3")
-    assert not good_expression("(1*2)+3")
-    assert   good_expression("(1+9*2*2*((6+7)*8+9))*8")
-    assert not good_expression("(1)+3")
-    assert   good_expression("(((2+3)*7+2)*9+3*2)*9")
-    assert not good_expression("((((1))))")
-    assert not good_expression("((5+2)*(7+9))")
-    assert not good_expression("(5*(7+9))")
-    assert not good_expression("((7+9)*5)")
-    assert not  good_expression("((1+9*2*2*((6+7)*8+9))*8)")
-    assert  good_expression("(5+3)*7*2+3")
-    assert not  good_expression("(2*3)+(5*2)")
-    assert not good_expression("((5+3)*4)")
-        
-    print ("all tests passed")
-    
+ 
 #####################################################
 def good_expression(d):    
     s=Stack()
     correct=False    
-    paren_anix=0
-    paren_kli=0
+    paren_open=0
+    paren_clo=0
     count=0
-    per1=False
-    per2=False
-    per3=False
-    per4=False
+    might=False
+    pass1=False 
+    pass2=False
+    pass3=False
+    pass4=False
     s.push(d[0])
     a=s.top()
     for i in range (0,len(d)):        
-        if  d[i]=="*" and a==")":
+        if  d[i]=="*" and a==")":               #after the parenthesis there is  a * sign. For the expression to be true there must be at least one + sign between the numbers in the parenthesis 
             correct=False
-            per1=True
-            print("if 1")
+            pass1=True 
             p=s.pop()
-            while(p!="("):                
+            while(p!="("):          #while the closing parenthesis is not found do the following                
                 if(p=="+"):
-                    print("p= ",p)
-                    correct=True                
+                    correct=True     #if at least one + sign is found that means the parenthesis is true           
                 p=s.pop()
                 a=s.top()
-            s.push(d[i])
-            paren_anix-=1
-        print("correct1: ", correct)   
-        if per1==True and correct==False:
+            s.push(d[i])            #push the sign to the stack
+            paren_open-=1           #the count for opening parenthesis decreases      
+         
+        if pass1==True and correct==False:      #if there is a * sign before the parenthesis but inside it there isn't a + sign that means the expression is bad. 
             return False
-        elif d[i]=="+" and a==")":
+        elif d[i]=="+" and a==")":          #if there is a + after the parenthesis. The program checks for * before the parenthesis and at least one + inside the parenthesis.
+            might=False
             correct=False
-            per4=True
-            print("if 1")
+            pass4=True            
             p=s.pop()
             while(p!="("):                
-                if(p=="+"):
-                    print("p= ",p)
+                if(p=="+"):                    
                     might=True            
                 p=s.pop()
                 a=s.top()
             c=s.pop()
-            paren_anix-=1
-            if c=="*" and might==True:
+            paren_open-=1
+            if c=="*" and might==True:      
                 correct=True
             else:
                 correct=False
             s.push(d[i])
             a=s.top()
             
-        if per4==True and correct==False:
+        if pass4==True and correct==False:   #if the parenthesis is bad(doen't contain + inside and a * outside) then the expression is also bad
             return False
           
-        elif d[i]==")" and i==len(d)-1:
+        elif d[i]==")" and i==len(d)-1:     #if the last element is a closing parenthesis it checks if there is a + inside the parenthesis and a * before of it, to be good.
              correct=False
-             per2=True
-             print("elif 2")
+             pass2=True             
              p=s.pop()             
-             while(p!="("):
-                print("p= ",p)
-                if(p=="+"):                    
+             while(p!="("):                
+                if(p=="+"):         #checks for at least one + inside the parenthesis                    
                     correct=True                    
                 p=s.pop()
                 a=s.top()                
              b=s.pop()
-             paren_anix-=1
-             print("b= ",b , "a= ",a)
-             if b=="+" or s.isEmpty()==True or b==")" or paren_anix>=1 :               
+             paren_open-=1             
+             if b=="+" or s.isEmpty()==True or b==")" or paren_open>=1 :     #if there is a + sign before , or redundant brackets,then the expression is bad          
                 correct=False
-             s.push(d[i])
-             #a=s.top()
-        print("correct2: ", correct, "a= ",a)  
-        if per2==True and correct==False:
+             s.push(d[i])             
+       
+        if pass2==True and correct==False:          #if there is a closing parenthesis but, no + sign inside and a * outside then the expression is bad
             return False
             
-        elif d[i]==")" and a==")" :
-            correct=False
-            per3=True
-            print("elif 3 ")
+        elif d[i]==")" and a==")" :                 #if there are 2 closing brackets make sure that there aren't redundant 
+            correct=False    
+            pass3=True            
             p=s.pop()
-            while(p!="("):
-                print("p= ",p)
+            while(p!="("):                
                 if(p=="+"):
                     correct=True
                 p=s.pop()
                 a=s.top()                
             if a=="(":                
                 correct=False
-            paren_anix-=1
-        print("correct4: ", correct, "a= ",a)          
-        if per3==True and correct==False:
+            paren_open-=1                  
+        if pass3==True and correct==False:  #if the parenthis is bad then the expression is also false 
             return False
         
-        else:                       
+        else:                               # add all the numbers and signs in the stack if none of the if statments are satisfied                    
             s.push(d[i])
-            a=s.top()
-            print("else me :", d[i], "a= ", a)
+            a=s.top()            
             if d[i]=="(":
-                paren_anix+=1
+                paren_open+=1
                 
             if d[i]==")":
-                paren_kli+=1
+                paren_clo+=1
             count+=1    
-    if count==len(d) and paren_kli==0 and paren_anix==0:
-        correct=True
-    print ("correct5 ",correct)
+    if count==len(d) and paren_clo==0 and paren_open==0:    #if there are no parenthesis then the expresion is always good 
+        correct=True    
     return correct
 
-print( good_expression("1*(5+3)*(1+3)*(9+8)"))
-#testq3()
 
--- q2_final.py
+++ q2_final.py
@@ -1,69 +1,46 @@
-import time
 def count_ephemeral(n1,n2,k):       
     start=time.time()
-    All_numbers=set()               # The numbers produced by q.
-    cacheSet={}                     # Stores all previous results !!!!!
+    All_numbers=set()               # The seqeuence of numbers produced by q.
+    cacheSet={}                     # A set contains all the numbers found and their result when raised to the kth power 
     count =0                        # Counts the epemeral numbers 
     if k==3:
-        HappyL={778,112,1198,1189,1234,1759,787,877,121}    #prestore values that are ephemeral with k=3 (lookup table)
-        UnhappyL={136,244,351,27,3,72,1458,24,42,53,18,81,513,729,792}          #prestore values that are not ephemenal with k=3 (lookup table)
+        ephemeralL={778,112,1198,1189,1234,1759,787,877,121}                        #prestore values that are ephemeral with k=3 (lookup table)
+        NonEphemeralL={136,244,351,27,3,72,1458,24,42,53,18,81,513,729,792}          #prestore values that are not ephemenal with k=3 (lookup table)
     elif k==2:
-        HappyL={7, 13, 19, 23, 28, 31, 32, 44, 49, 68, 70, 79, 82, 86, 91, 94, 97,129,133, 139, 167, 176, 188,192, 193,219,203,208}
-        UnhappyL={2,4,16, 37,58,89,145,42,24,98,85,61,73,5,25}
+        ephemeralL={7, 13, 19, 23, 28, 31, 32, 44, 49, 68, 70, 79, 82, 86, 91, 94, 97,129,133, 139, 167, 176, 188,192, 193,219,203,208}
+        NonEphemeralL={2,4,16, 37,58,89,145,42,24,98,85,61,73,5,25}
     else:
-        HappyL= {11231,233869,444797,44688,100,10000,12131}
-        UnhappyL= {2,13139, 6725, 4338, 4514, 1138, 4179, 9219,16,14,434,2433,228,5729,19619,11969}
-    for q in range(n1,n2):
+        ephemeralL= {11231,233869,444797,44688,100,10000,12131}
+        NonEphemeralL= {2,13139, 6725, 4338, 4514, 1138, 4179, 9219,16,14,434,2433,228,5729,19619,11969}
+    for q in range(n1,n2):        
         All_numbers.add(q)       
         new_num=q
-        str_num=q
-        happyFlag = False
+        str_num=q                           #the number that is going to be convertated into a string
+        ephFlag = False
         while new_num!=1:
             if new_num in cacheSet:         # Checks if the result has come up before, if so, no calculations need to be done.
-                new_num = cacheSet[new_num] # sets the number to it's result!!!!
+                new_num = cacheSet[new_num] # sets the number to it's result
             else:
-                Anchor = new_num            #Kame ena print NA DIS TI ENI !!!!!             
+                Anchor = new_num                                                 
                 new_num=0                
                 stringOfNum = str(str_num)    
-                for num in map(int, stringOfNum):                                 
+                for num in map(int, stringOfNum):  #takes every digit raise the to the k power and creates the new number.                                 
                     new_num +=num**k
-                cacheSet[Anchor] = new_num # Adds the result into the cache set                
-            if  new_num in All_numbers: 
+                cacheSet[Anchor] = new_num      # Adds the result into the cache set                
+            if  new_num in All_numbers:         #checks if q produced the same number twice
                 break
-            if  new_num in HappyL:
-                happyFlag = True    #Happy Flag= breaks because the  number is found in the HappyList
+            if  new_num in ephemeralL :
+                ephFlag = True              # breaks because the  number is found in the ephemeral list
                 break          
-            if  new_num in UnhappyL:
+            if  new_num in NonEphemeralL:
                 break
             
             All_numbers.add(new_num)   
-            str_num=new_num             
+            str_num=new_num          #the number that we have to break into is digits is the new_num          
                   
-        if new_num==1 or happyFlag:
+        if new_num==1 or ephFlag:
             count+=1
-            All_numbers.clear()         #clears the list 
-    print(time.time() - start)  
+            All_numbers.clear()         #clears the list
     return count
 
 
-def q2test():
-    """tests for the function count_ephemeral"""
-    correct = True
-    result = count_ephemeral(1, 10, 2)
-    if result != 2:
-        correct = False
-        print("test failed for n1=1, n2=10, k=2; correct result is 2, result obtained was ", result)
-    result = count_ephemeral(1000, 10000, 3)
-    if result != 91:
-        correct = False
-        print("test failed for n1=1000, n2=10000, k=3; correct result is 91, result obtained was ", result)
-    result = count_ephemeral(123456, 654321, 4)
-    if result != 376:
-        correct = False
-        print("test failed for n1=123456, n2=654321, k=4; correct result is 376, result obtained was ", result)
-    if correct:
-        print("all tests passed")
-
-#print( count_ephemeral(1, 10, 2))
-
-#q2test()
