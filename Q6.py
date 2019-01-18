4f9c073e64150c4054b77d81b3fe075598a0b6a5
PanayiotaK <panagiota.konstantinou@durham.ac.uk>
2019-01-18 02:21:48 +0000

    final

--- Q6.py
+++ Q6.py
@@ -1,83 +1,50 @@
-count=0
 def Merge(lefts,rights):
-    result=[]
-    global count
-    while len(lefts)>0 or len(rights)>0 :
-        #print("left length ", len(lefts))
-        #print("right length ", len(rights))
-        
-        if len(lefts)>0 and len(rights)>0:
-            count+=1
-            if lefts[0]< rights[0]:
-                count+=1
-                result.append(rights[0])
+    result=[]    
+    while len(lefts)>0 or len(rights)>0 :        
+        if len(lefts)>0 and len(rights)>0:          #if both lists still have elements inside them        
+            if lefts[0]< rights[0]:                 #and the first element on the left list is smaller than the first element of the right list             
+                result.append(rights[0])            #the  final result will be append with the right element and remove it from the rights lists 
                 rights.remove(rights[0])
-            else:
-                count+=1
-                result.append(lefts[0])
+            else:               
+                result.append(lefts[0])             #else the results list will be append with the element of the left list 
                 lefts.remove(lefts[0])
 
-        elif len(lefts)>0 :
-                count+=1
+        elif len(lefts)>0 :                         #the right list is empty but the left list is not     
                 result.extend(lefts)
                 lefts.clear()
       
-        else:
-            #if len(lefts)==0 and  len(rights)>0 :
-                #print("else ")
-                count+=1
-                result.extend(rights)
-                rights.clear()
-        
-        count+=1
-        #print("lefts ",lefts)
-        #print("result ",result)
+        else:   
+            result.extend(rights)
+            rights.clear()
     return result
 
-def SelectionSort(d,n):
-    global count  
+def SelectionSort(d,n):      
     for i in range(n):
-        element=d[i]
-        pos=i        
-        count+=1
-        for j in range(i+1,n):
-            count+=2
+        element=d[i]        #the smallest value in the list
+        pos=i               #the position of the smallest element found
+        for j in range(i+1,n):            
             if d[j]<element:                
                 element=d[j]
                 pos=j
-        temp=d[i]
+        temp=d[i]               #swaps the 2 elements
         d[i]=d[pos]
-        d[pos]=temp       
-     
-    #print("d =",d)
-    
+        d[pos]=temp  
     d.reverse()
             
 
-def MergeSort(m):
-    global count
-    if len(m)<=4:
-        SelectionSort(m,len(m))
-        count+=1
+def MergeSort(m):    
+    if len(m)<=4:                 #if the length of the list is 4 or less than 4.The list will be sorted with Selection Sort
+        SelectionSort(m,len(m))       
         return m
-    middle=len(m)//2
-    left=[]
-    right=[]
-    leftsorted=[]
-    rightsorted=[]
-    left=m[:middle]
-    #print("left= ", left)
-    right=m[middle:len(m)]
-    #print("right ",right)
-    leftsorted=MergeSort(left)
-    #print("leftsorted= ",leftsorted)
-    rightsorted=MergeSort(right)
-    #print("rightsorted= ",rightsorted)
-    
-    print("count ", count)
+    middle=len(m)//2    #finds the middle of the list       
+    left=[]             #the left list 
+    right=[]            #the right list 
+    leftsorted=[]       #the sorted left list
+    rightsorted=[]      #the sorted right list 
+    left=m[:middle]   
+    right=m[middle:len(m)]    
+    leftsorted=MergeSort(left)    
+    rightsorted=MergeSort(right)    
     return Merge(leftsorted,rightsorted)
             
-                
 
-
-print(MergeSort([7,5,9,3,6]))
